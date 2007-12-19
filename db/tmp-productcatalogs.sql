42670d0f83cdcb79d2c6967d1305bc2d0b99f0c3
Michael Calmer <mc@suse.de>
2007-12-19 14:57:12 +0000

    tmp content for ProductCatalogs table

---
+++ db/tmp-productcatalogs.sql
@@ -0,0 +1,140 @@
+-- create table ProductCatalogs(ProductID   integer NOT NULL,
+--                              CatalogID   integer NOT NULL,
+--                              Optional    text DEFAULT 'N',
+--                              PRIMARY KEY(ProductID, CatalogID)
+--                            );
+
+-- SLED-10-SP1 i386
+insert into ProductCatalogs(ProductID, CatalogID, Optional)
+ values(434, 13, 'N');
+insert into ProductCatalogs(ProductID, CatalogID, Optional)
+ values(433, 13, 'N');
+insert into ProductCatalogs(ProductID, CatalogID, Optional)
+ values(432, 13, 'N');
+insert into ProductCatalogs(ProductID, CatalogID, Optional)
+ values(431, 13, 'N');
+
+-- SLED-10-SP1 x86_64
+insert into ProductCatalogs(ProductID, CatalogID, Optional)
+ values(435, 14, 'N');
+
+-- SLES-10-SP1 i386
+insert into ProductCatalogs(ProductID, CatalogID, Optional)
+ values(436, 8, 'N');
+insert into ProductCatalogs(ProductID, CatalogID, Optional)
+ values(437, 8, 'N');
+insert into ProductCatalogs(ProductID, CatalogID, Optional)
+ values(438, 8, 'N');
+insert into ProductCatalogs(ProductID, CatalogID, Optional)
+ values(439, 8, 'N');
+
+-- SLES-10-SP1 x86_64
+insert into ProductCatalogs(ProductID, CatalogID, Optional)
+ values(440, 12, 'N');
+
+-- SLES-10-SP1 ia64
+insert into ProductCatalogs(ProductID, CatalogID, Optional)
+ values(443, 9, 'N');
+
+-- SLES-10-SP1 ppc
+insert into ProductCatalogs(ProductID, CatalogID, Optional)
+ values(441, 10, 'N');
+insert into ProductCatalogs(ProductID, CatalogID, Optional)
+ values(442, 10, 'N');
+
+-- SLES-10-SP1 s390
+insert into ProductCatalogs(ProductID, CatalogID, Optional)
+ values(444, 11, 'N');
+insert into ProductCatalogs(ProductID, CatalogID, Optional)
+ values(445, 11, 'N');
+
+
+
+
+
+
+
+
+
+
+
+
+-- SLED-10-SP1 i386 Online
+insert into ProductCatalogs(ProductID, CatalogID, Optional)
+ values(446, 13, 'N');
+insert into ProductCatalogs(ProductID, CatalogID, Optional)
+ values(447, 13, 'N');
+insert into ProductCatalogs(ProductID, CatalogID, Optional)
+ values(448, 13, 'N');
+insert into ProductCatalogs(ProductID, CatalogID, Optional)
+ values(449, 13, 'N');
+insert into ProductCatalogs(ProductID, CatalogID, Optional)
+ values(446, 6, 'N');
+insert into ProductCatalogs(ProductID, CatalogID, Optional)
+ values(447, 6, 'N');
+insert into ProductCatalogs(ProductID, CatalogID, Optional)
+ values(448, 6, 'N');
+insert into ProductCatalogs(ProductID, CatalogID, Optional)
+ values(449, 6, 'N');
+
+-- SLED-10-SP1 x86_64 Online
+insert into ProductCatalogs(ProductID, CatalogID, Optional)
+ values(450, 14, 'N');
+insert into ProductCatalogs(ProductID, CatalogID, Optional)
+ values(450, 7, 'N');
+
+-- SLES-10-SP1 i386 Online
+insert into ProductCatalogs(ProductID, CatalogID, Optional)
+ values(451, 8, 'N');
+insert into ProductCatalogs(ProductID, CatalogID, Optional)
+ values(452, 8, 'N');
+insert into ProductCatalogs(ProductID, CatalogID, Optional)
+ values(453, 8, 'N');
+insert into ProductCatalogs(ProductID, CatalogID, Optional)
+ values(454, 8, 'N');
+
+insert into ProductCatalogs(ProductID, CatalogID, Optional)
+ values(451, 1, 'N');
+insert into ProductCatalogs(ProductID, CatalogID, Optional)
+ values(452, 1, 'N');
+insert into ProductCatalogs(ProductID, CatalogID, Optional)
+ values(453, 1, 'N');
+insert into ProductCatalogs(ProductID, CatalogID, Optional)
+ values(454, 1, 'N');
+
+
+-- SLES-10-SP1 x86_64 Online
+insert into ProductCatalogs(ProductID, CatalogID, Optional)
+ values(455, 12, 'N');
+
+insert into ProductCatalogs(ProductID, CatalogID, Optional)
+ values(455, 5, 'N');
+
+-- SLES-10-SP1 ia64 Online
+insert into ProductCatalogs(ProductID, CatalogID, Optional)
+ values(458, 9, 'N');
+
+insert into ProductCatalogs(ProductID, CatalogID, Optional)
+ values(458, 2, 'N');
+
+-- SLES-10-SP1 ppc Online
+insert into ProductCatalogs(ProductID, CatalogID, Optional)
+ values(456, 10, 'N');
+insert into ProductCatalogs(ProductID, CatalogID, Optional)
+ values(457, 10, 'N');
+
+insert into ProductCatalogs(ProductID, CatalogID, Optional)
+ values(456, 3, 'N');
+insert into ProductCatalogs(ProductID, CatalogID, Optional)
+ values(457, 3, 'N');
+
+-- SLES-10-SP1 s390 Online
+insert into ProductCatalogs(ProductID, CatalogID, Optional)
+ values(459, 11, 'N');
+insert into ProductCatalogs(ProductID, CatalogID, Optional)
+ values(460, 11, 'N');
+
+insert into ProductCatalogs(ProductID, CatalogID, Optional)
+ values(459, 4, 'N');
+insert into ProductCatalogs(ProductID, CatalogID, Optional)
+ values(460, 4, 'N');
