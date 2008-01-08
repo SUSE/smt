316440f7ac675984f54817fff20c500e58cdc8e2
Duncan Mac-Vicar P <dmacvicar@suse.de>
2008-01-08 15:51:59 +0000

    update the test

--- tests/YEP/Mirror/RpmMd.pl
+++ tests/YEP/Mirror/RpmMd.pl
@@ -1,4 +1,5 @@
 #!/usr/bin/env perl
+use File::Temp;
 
 BEGIN {
     push @INC, "/space/git/suse/yep/www/perl-lib";
@@ -7,7 +8,12 @@ BEGIN {
 use YEP::Mirror::RpmMd;
 use Test::Simple tests => 1;
 
-$mirror = YEP::Mirror::RpmMd->new();
-$mirror->uri( 'http://download.opensuse.org/repositories/home:/dmacvicar/openSUSE_10.3/' );
-ok($mirror->mirrorTo( "./testdata/rpmmdtest/" ) == 0);
+my $url = 'http://download.opensuse.org/repositories/home:/dmacvicar/openSUSE_10.3/';
+my $tempdir = File::Temp::tempdir();
+print STDERR "Saving $url to $tempdir\n";
 
+$mirror = YEP::Mirror::RpmMd->new();
+$mirror->uri( $url );
+ok($mirror->mirrorTo( $tempdir ) == 0);
+
+ok($mirror->verify($tempdir) == 0);
