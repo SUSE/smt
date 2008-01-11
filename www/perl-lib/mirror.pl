16958a8859bbb3a077ae157ba7f70a16837922aa
Michael Calmer <mc@suse.de>
2008-01-11 12:12:55 +0000

    dropped - not needed anymore

--- www/perl-lib/mirror.pl
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env perl
-
-use YEP::Mirror::RpmMd;
-
-$mirror = YEP::Mirror::RpmMd->new();
-$mirror->uri( "http://download.opensuse.org/repositories/home:/dmacvicar/openSUSE_10.3" );
-$mirror->mirrorTo( "/space/git/suse/yep/www/repo", { urltree => 0 } );
-
--- www/perl-lib/mirrorNU.pl
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env perl
-
-use YEP::Mirror::NU;
-
-$mirror = YEP::Mirror::NU->new();
-$mirror->uri( 'https://MIRRORUSER:MIRRIRPASSWORD@nu.novell.com');
-$mirror->mirrorTo( "/srv/www/htdocs/", { urltree => 0 } );
-
