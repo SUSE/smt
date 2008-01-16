bbc1a125330f391e9d422d879d5bae4c44180350
Michael Calmer <mc@suse.de>
2008-01-16 14:24:00 +0000

    first working version of registration server

--- www/perl-lib/YEP/Registration.pm
+++ www/perl-lib/YEP/Registration.pm
@@ -13,13 +13,9 @@ use YEP::Utils;
 
 use Data::Dumper;
 use DBI;
-#use HTML::Entities;
 use XML::Writer;
 use XML::Parser;
 use XML::Bare;
-
-open(LOG, ">> /tmp/registration.log") or die "Cannot open logfile: $!";
-
 
 sub handler {
     my $r = shift;
@@ -79,32 +75,33 @@ sub register
 
     my $data = YEP::Utils::read_post($r);
     my $dbh = YEP::Utils::db_connect();
+    if(!$dbh)
+    {
+        die "Cannot open Database";
+    }
     
+    my $regroot = { ACCEPTOPT => 1, CURRENTELEMENT => "", PRODUCTATTR => {}, register => {}};
+    my $regparser = XML::Parser->new( Handlers =>
+                                      { Start => sub { reg_handle_start_tag($regroot, @_) },
+                                        Char  => sub { reg_handle_char_tag($regroot, @_) },
+                                        End   => sub { reg_handle_end_tag($regroot, @_) }
+                                      });
+
+    $regparser->parse( $data );
+
     my $needinfo = YEP::Registration::parseFromProducts($r, $dbh, $data, "NEEDINFO");
 
-    my $parser1 = XML::Bare->new( text => $data );
-    my $regroot = $parser1->parse();
-
-    # second try 
+    $r->log_error("REGROOT:".Data::Dumper->Dump([$regroot]));
 
     my $dat = { NEWINFO => "", 
                 CACHE => [],
                 WRITECACHE => 0,
                 INFOCOUNT => 0, 
                 REGISTER => $regroot,
-                PARSER => $parser1,
                 PARAMDEPTH => 0,
                 PARAMISMAND => 0,
                 R => $r
                };
-    if(exists $regroot->{register}->{accept} && $regroot->{register}->{accept} eq "mandatory")
-    {
-        $dat->{ACCEPTOPT} = 0;
-    }
-    else
-    {
-        $dat->{ACCEPTOPT} = 1;
-    }
     
     my $parser = XML::Parser->new( Handlers =>
                                    { Start=> sub { nif_handle_start_tag($dat, @_) },
@@ -123,10 +120,116 @@ sub register
     {
         # we have all data; store it and send <zmdconfig>
 
+        # remove all db values from an old registration of this GUID
+
+        YEP::Registration::cleanOldRegistration($r, $dbh, $regroot->{register}->{guid});
+
+        # insert new registration data
+
+        YEP::Registration::insertRegistration($r, $dbh, $regroot);
+
+        # get the catalogs
+
+        my $catalogs = YEP::Registration::findCatalogs($r, $dbh, $regroot->{register}->{guid});
+
+        # send new <zmdconfig>
+
+        my $zmdconfig = YEP::Registration::buildZmdConfig($r, $regroot->{register}->{guid}, $catalogs);
+
+        $r->log_error("ZMDCONFIG: $zmdconfig");
+
+        print $zmdconfig;
     }
     
     return;
 }
+
+sub reg_handle_start_tag
+{
+    my $data = shift;
+    my( $expat, $element, %attrs ) = @_;
+
+    if(lc($element) eq "param")
+    {
+        if(exists $attrs{id} && defined $attrs{id} && $attrs{id} ne "")
+        {
+            $data->{CURRENTELEMENT} = $attrs{id};
+            # empty params are allowed, so we create the node here
+            $data->{register}->{$data->{CURRENTELEMENT}} = "";
+        }
+    }
+    elsif(lc($element) eq "product")
+    {
+        $data->{CURRENTELEMENT} = lc($element);
+        $data->{PRODUCTATTR} = \%attrs;
+    }
+    elsif(lc($element) eq "register")
+    {
+        if(exists $attrs{accept} && defined $attrs{accept} && lc($attrs{accept}) eq "mandatory")
+        {
+            $data->{ACCEPTOPT} = 0;
+        }        
+    }
+    elsif(lc($element) eq "host")
+    {
+        $data->{CURRENTELEMENT} = lc($element);
+        # empty host is allowed, so we create the node here
+        $data->{register}->{$data->{CURRENTELEMENT}} = "";
+    }
+    else
+    {
+        $data->{CURRENTELEMENT} = lc($element);
+    }
+}
+
+sub reg_handle_char_tag
+{
+    my $data = shift;
+    my( $expat, $string) = @_;
+
+    if($data->{CURRENTELEMENT} ne "" && $data->{CURRENTELEMENT} ne "product")
+    {
+        if(exists $data->{register}->{$data->{CURRENTELEMENT}} &&
+           defined $data->{register}->{$data->{CURRENTELEMENT}} &&
+           $data->{register}->{$data->{CURRENTELEMENT}} ne "")
+        {
+            $data->{register}->{$data->{CURRENTELEMENT}} .= $string;
+        }
+        else
+        {
+            $data->{register}->{$data->{CURRENTELEMENT}} = $string;
+        }
+    }
+    elsif($data->{CURRENTELEMENT} eq "product")
+    {
+        if(exists $data->{PRODUCTATTR}->{name} &&
+           defined $data->{PRODUCTATTR}->{name} &&
+           $data->{PRODUCTATTR}->{name} ne "")
+        {
+            $data->{PRODUCTATTR}->{name} .= $string;
+        }
+        else
+        {
+            $data->{PRODUCTATTR}->{name} = $string;
+        }
+    }
+}
+
+sub reg_handle_end_tag
+{
+    my $data = shift;
+    my( $expat, $element) = @_;
+
+    if(lc($element) eq "product")
+    {
+        if(!exists $data->{register}->{product})
+        {
+            $data->{register}->{product} = [];
+        }
+        push @{$data->{register}->{product}}, $data->{PRODUCTATTR};
+    }
+}
+
 
 sub writeElement
 {
@@ -209,15 +312,20 @@ sub nif_handle_start_tag
         
         if(exists $attrs{id} && defined $attrs{id})
         {
-            my $pnode = $data->{PARSER}->find_node($data->{REGISTER}->{register}, "param", id => $attrs{id});
-            if($pnode)
+            #my $pnode = $data->{PARSER}->find_node($data->{REGISTER}->{register}, "param", id => $attrs{id});
+            #if($pnode)
+            #{
+            #    # skip this, it is already there
+            #}
+
+            if(exists $data->{REGISTER}->{register}->{$attrs{id}})
             {
                 # skip this, it is already there
             }
             elsif(exists $attrs{command} && defined $attrs{command})
             {
                 
-                if($data->{ACCEPTOPT} || (!$data->{ACCEPTOPT} && $data->{PARAMISMAND}))
+                if($data->{REGISTER}->{ACCEPTOPT} || (!$data->{REGISTER}->{ACCEPTOPT} && $data->{PARAMISMAND}))
                 {
                     # we do not have a value for this command
                     # if we accept optional => write it
@@ -683,7 +791,292 @@ sub joinNeedinfolist
     return $parser->xml( $root );
 }
 
+sub cleanOldRegistration
+{
+    my $r    = shift;
+    my $dbh  = shift;
+    my $guid = shift;
+    
+    if(!$dbh)
+    {
+        $r->log_error("Something is wrong with the database handle");
+    }
+    
 
+    my $statement = sprintf("DELETE from MachineData where GUID=%s", $dbh->quote($guid));
+    $r->log_error("STATEMENT: $statement");
+    eval {
+        $dbh->do($statement);
+    };
+    if($@)
+    {
+        $r->log_error("DBERROR: ".$dbh->errstr);
+    }
+    
+
+    $statement = sprintf("DELETE from Registration where GUID=%s", $dbh->quote($guid));
+    $r->log_error("STATEMENT: $statement");
+    $dbh->do($statement);
+
+    return;
+}
+
+sub insertRegistration
+{
+    my $r       = shift;
+    my $dbh     = shift;
+    my $regdata = shift;
+
+    my @list = ();
+    foreach my $phash (@{$regdata->{register}->{product}})
+    {
+        foreach my $cnt (1..3)
+        {
+            my $statement = "SELECT PRODUCTDATAID FROM Products where ";
+
+            $statement .= "PRODUCTLOWER = ".$dbh->quote(lc($phash->{name}))." AND ";
+
+            $statement .= "VERSIONLOWER ";
+
+            if(!defined $phash->{version})
+            {
+                $statement .= "IS NULL AND ";
+            }
+            else
+            {
+                $statement .= "= ".$dbh->quote(lc($phash->{version}))." AND ";
+            }
+            
+            $statement .= "ARCHLOWER ";
+            
+            if(!defined $phash->{arch})
+            {
+                $statement .= "IS NULL AND ";
+            }
+            else
+            {
+                $statement .= "= ".$dbh->quote(lc($phash->{arch}))." AND ";
+            }
+            
+            $statement .= "RELEASELOWER ";
+
+            if(!defined $phash->{release})
+            {
+                $statement .= "IS NULL";
+            }
+            else
+            {
+                $statement .= "= ".$dbh->quote(lc($phash->{release}));
+            }
+                        
+            #$r->log_error("STATEMENT: $statement");
+            
+            my $pl = $dbh->selectall_arrayref($statement, {Slice => {}});
+            
+            #$r->log_error("RESULT: ".Data::Dumper->Dump([$pl]));
+            #$r->log_error("RESULT: not defined ") if(!defined $pl);
+            #$r->log_error("RESULT: empty ") if(@$pl == 0);
+
+            if(@$pl == 0 && $cnt == 1)
+            {
+                $phash->{release} = undef;
+            }
+            elsif(@$pl == 0 && $cnt == 2)
+            {
+                $phash->{arch} = undef;
+            }
+            elsif(@$pl == 0 && $cnt == 3)
+            {
+                $phash->{version} = undef;
+            }
+            elsif(@$pl == 1 && exists $pl->[0]->{PRODUCTDATAID})
+            {
+                push @list, $pl->[0]->{PRODUCTDATAID};
+                last;
+            }
+        }
+    }
+   
+    foreach my $pnum (@list)
+    {
+        my $statement = sprintf("INSERT into Registration (GUID, PRODUCTID) VALUES (%s, %s)", 
+                                $dbh->quote($regdata->{register}->{guid}), $pnum);
+        $r->log_error("STATEMENT: $statement");
+        $dbh->do($statement);
+    }
+    
+    foreach my $key (keys %{$regdata->{register}})
+    {
+        next if($key eq "guid" || $key eq "product" || $key eq "mirrors");
+        
+        my $statement = sprintf("INSERT into MachineData (GUID, KEY, VALUE) VALUES (%s, %s, %s)",
+                                $dbh->quote($regdata->{register}->{guid}), 
+                                $dbh->quote($key),
+                                $dbh->quote($regdata->{register}->{$key}));
+        $r->log_error("STATEMENT: $statement");
+        $dbh->do($statement);
+    }
+    return;
+}
+
+sub findCatalogs
+{
+    my $r    = shift;
+    my $dbh  = shift;
+    my $guid = shift;
+
+    my $result = {};
+
+    # get productid for this guid
+
+    my $statement = sprintf("SELECT PRODUCTID from Registration WHERE GUID=%s", $dbh->quote($guid));
+    $r->log_error("STATEMENT: $statement");
+    
+    my $productids = $dbh->selectcol_arrayref($statement);
+    
+    # get product dependencies
+
+    my $pidhash = {};
+    
+    foreach my $parent (@{$productids})
+    {
+        $pidhash->{$parent} = 1;
+        
+        $statement = "SELECT CHILD_PRODUCT_ID from ProductDependencies WHERE PARENT_PRODUCT_ID=$parent";
+        $r->log_error("STATEMENT: $statement");
+
+        my $childs = $dbh->selectcol_arrayref($statement);
+        foreach my $child (@{$childs})
+        {
+            $pidhash->{$child} = 1;
+        }
+    }
+    
+    # get product catalogs
+
+    $statement = "SELECT CATALOGID from ProductCatalogs WHERE OPTIONAL='N' AND PRODUCTDATAID ";
+    
+    if(keys %{$pidhash} > 1)
+    {
+        $statement .= "IN (".join(",", keys %{$pidhash}).")";
+    }
+    elsif(keys %{$pidhash} == 1)
+    {
+        $statement .= "= '".join("", keys %{$pidhash})."'";
+    }
+    else
+    {
+        # This should not happen
+        $r->log_error("No productids found");
+        return $result;
+    }
+    
+    $r->log_error("STATEMENT: $statement");
+    
+    my $catalogs = $dbh->selectcol_arrayref($statement);
+
+    # get catalog values (only for the once we DOMIRROR)
+
+    $statement = "SELECT CATALOGID, NAME, DESCRIPTION, TARGET, LOCALPATH, CATALOGTYPE from Catalogs WHERE CATALOGID ";
+
+    if(@{$catalogs} > 1)
+    {
+        $statement .= "IN ('".join("','", @{$catalogs})."')";
+    }
+    elsif(@{$catalogs} == 1)
+    {
+        $statement .= "= '".$catalogs->[0]."'";
+    }
+    else
+    {
+        $r->log_error("No catalogs for these products");
+        return $result;
+    }
+    
+    $r->log_error("STATEMENT: $statement");
+
+    $result = $dbh->selectall_hashref($statement, "CATALOGID");
+
+    $r->log_error("RESULT: ".Data::Dumper->Dump([$result]));
+
+    return $result;
+}
+
+sub buildZmdConfig
+{
+    my $r         = shift;
+    my $guid      = shift;
+    my $catalogs  = shift;
+
+    my $cfg = new Config::IniFiles( -file => "/etc/yep.conf" );
+    if(!defined $cfg)
+    {
+        # FIXME: is die correct here?
+        die "Cannot read the YEP configuration file: ".@Config::IniFiles::errors;
+    }
+    
+    my $LocalNUUrl = $cfg->val('REG', 'url');
+
+    my $output = "";
+    my $writer = new XML::Writer(OUTPUT => \$output);
+
+    $writer->xmlDecl("UTF-8");
+    $writer->startTag("zmdconfig", 
+                      "xmlns" => "http://www.novell.com/xml/center/regsvc-1_0",
+                      "lang"  => "en");
+    
+    $writer->startTag("guid");
+    $writer->characters($guid);
+    $writer->endTag("guid");
+    
+    # first write all catalogs of type NU
+
+    $writer->startTag("service", 
+                      "id"          => "local_nu_server",
+                      "description" => "Local NU Server",
+                      "type"        => "nu");
+    $writer->startTag("param", "id" => "url");
+    $writer->characters($LocalNUUrl);
+    $writer->endTag("param");
+    
+    foreach my $cat (keys %{$catalogs})
+    {
+        next if($catalogs->{$cat}->{CATALOGTYPE} ne "nu");
+        
+        $writer->startTag("param", 
+                          "name" => "catalog",
+                          "url"  => "$LocalNUUrl/repo/".$catalogs->{$cat}->{LOCALPATH});
+        $writer->characters($catalogs->{$cat}->{NAME});
+        $writer->endTag("param");
+    }
+    $writer->endTag("service");
+    
+    # and now the YUM Repositories
+
+    foreach my $cat (keys %{$catalogs})
+    {
+        next if($catalogs->{$cat}->{CATALOGTYPE} ne "yum");
+
+        $writer->startTag("service", 
+                          "id"          => $catalogs->{$cat}->{NAME},
+                          "description" => $catalogs->{$cat}->{DESCRIPTION},
+                          "type"        => "zypp");
+        $writer->startTag("param", "id" => "url");
+        $writer->characters("$LocalNUUrl/repo/".$catalogs->{$cat}->{LOCALPATH});
+        $writer->endTag("param");
+        
+
+        $writer->startTag("param", "name" => "catalog");
+        $writer->characters($catalogs->{$cat}->{NAME});
+        $writer->endTag("param");
+
+        $writer->endTag("service");
+    }
+
+    $writer->endTag("zmdconfig");
+
+    return $output;
+}
 
 
 1;
