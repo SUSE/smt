d1aea9fb3b6fe2fe9bf2c4e0cdf19b1fd8a3b334
Michael Calmer <mc@suse.de>
2008-01-27 17:40:59 +0000

    SP2 i386 catalogs

--- db/tmp-productcatalogs.sql
+++ db/tmp-productcatalogs.sql
@@ -48,97 +48,6 @@ insert into ProductCatalogs(PRODUCTDATAID, CATALOGID, OPTIONAL)
 insert into ProductCatalogs(PRODUCTDATAID, CATALOGID, OPTIONAL)
  values(445, "336e58595d8b93b88a582493c67e389330429012", 'N');
 
-
-
-
-
-
-
-
-
-
-
-
--- SLED-10-SP1 i386 Online
-insert into ProductCatalogs(PRODUCTDATAID, CATALOGID, OPTIONAL)
- values(446, "9396b5a1b07087f5a78bd8ffdf2aad7fd2815ef2", 'N');
-insert into ProductCatalogs(PRODUCTDATAID, CATALOGID, OPTIONAL)
- values(447, "9396b5a1b07087f5a78bd8ffdf2aad7fd2815ef2", 'N');
-insert into ProductCatalogs(PRODUCTDATAID, CATALOGID, OPTIONAL)
- values(448, "9396b5a1b07087f5a78bd8ffdf2aad7fd2815ef2", 'N');
-insert into ProductCatalogs(PRODUCTDATAID, CATALOGID, OPTIONAL)
- values(449, "9396b5a1b07087f5a78bd8ffdf2aad7fd2815ef2", 'N');
-insert into ProductCatalogs(PRODUCTDATAID, CATALOGID, OPTIONAL)
- values(446, "a6af7e6f0b08dad8fb5907855c76743d47542f64", 'N');
-insert into ProductCatalogs(PRODUCTDATAID, CATALOGID, OPTIONAL)
- values(447, "a6af7e6f0b08dad8fb5907855c76743d47542f64", 'N');
-insert into ProductCatalogs(PRODUCTDATAID, CATALOGID, OPTIONAL)
- values(448, "a6af7e6f0b08dad8fb5907855c76743d47542f64", 'N');
-insert into ProductCatalogs(PRODUCTDATAID, CATALOGID, OPTIONAL)
- values(449, "a6af7e6f0b08dad8fb5907855c76743d47542f64", 'N');
-
--- SLED-10-SP1 x86_64 Online
-insert into ProductCatalogs(PRODUCTDATAID, CATALOGID, OPTIONAL)
- values(450, "59d9554c7a5a0e8bb098c5546547749753673557", 'N');
-insert into ProductCatalogs(PRODUCTDATAID, CATALOGID, OPTIONAL)
- values(450, "ae37e2c9a651c462fe919e6c0b7032a94f697537", 'N');
-
--- SLES-10-SP1 i386 Online
-insert into ProductCatalogs(PRODUCTDATAID, CATALOGID, OPTIONAL)
- values(451, "b834151cb82cb1068d4fb7d1d0a57199897c593c", 'N');
-insert into ProductCatalogs(PRODUCTDATAID, CATALOGID, OPTIONAL)
- values(452, "b834151cb82cb1068d4fb7d1d0a57199897c593c", 'N');
-insert into ProductCatalogs(PRODUCTDATAID, CATALOGID, OPTIONAL)
- values(453, "b834151cb82cb1068d4fb7d1d0a57199897c593c", 'N');
-insert into ProductCatalogs(PRODUCTDATAID, CATALOGID, OPTIONAL)
- values(454, "b834151cb82cb1068d4fb7d1d0a57199897c593c", 'N');
-
-insert into ProductCatalogs(PRODUCTDATAID, CATALOGID, OPTIONAL)
- values(451, "cbf17366895bac0574998a90b354075a314d886d", 'N');
-insert into ProductCatalogs(PRODUCTDATAID, CATALOGID, OPTIONAL)
- values(452, "cbf17366895bac0574998a90b354075a314d886d", 'N');
-insert into ProductCatalogs(PRODUCTDATAID, CATALOGID, OPTIONAL)
- values(453, "cbf17366895bac0574998a90b354075a314d886d", 'N');
-insert into ProductCatalogs(PRODUCTDATAID, CATALOGID, OPTIONAL)
- values(454, "cbf17366895bac0574998a90b354075a314d886d", 'N');
-
-
--- SLES-10-SP1 x86_64 Online
-insert into ProductCatalogs(PRODUCTDATAID, CATALOGID, OPTIONAL)
- values(455, "0fc1f631248dedc777ca01a23364f902d1e10cbb", 'N');
-
-insert into ProductCatalogs(PRODUCTDATAID, CATALOGID, OPTIONAL)
- values(455, "e5555c2bc7040012e84324811daac59136908ab8", 'N');
-
--- SLES-10-SP1 ia64 Online
-insert into ProductCatalogs(PRODUCTDATAID, CATALOGID, OPTIONAL)
- values(458, "c3d026cf4a4b97893b05b704738fd44b3a871a0b", 'N');
-
-insert into ProductCatalogs(PRODUCTDATAID, CATALOGID, OPTIONAL)
- values(458, "b505335419ac91b34661681e9e3a3bd2480272d0", 'N');
-
--- SLES-10-SP1 ppc Online
-insert into ProductCatalogs(PRODUCTDATAID, CATALOGID, OPTIONAL)
- values(456, "fb7bf021afb2c58111128f3c2cbebfd97f2c1fc4", 'N');
-insert into ProductCatalogs(PRODUCTDATAID, CATALOGID, OPTIONAL)
- values(457, "fb7bf021afb2c58111128f3c2cbebfd97f2c1fc4", 'N');
-
-insert into ProductCatalogs(PRODUCTDATAID, CATALOGID, OPTIONAL)
- values(456, "5ecbc49855635803ad1ede5bf2c2586f70e504fb", 'N');
-insert into ProductCatalogs(PRODUCTDATAID, CATALOGID, OPTIONAL)
- values(457, "5ecbc49855635803ad1ede5bf2c2586f70e504fb", 'N');
-
--- SLES-10-SP1 s390 Online
-insert into ProductCatalogs(PRODUCTDATAID, CATALOGID, OPTIONAL)
- values(459, "336e58595d8b93b88a582493c67e389330429012", 'N');
-insert into ProductCatalogs(PRODUCTDATAID, CATALOGID, OPTIONAL)
- values(460, "336e58595d8b93b88a582493c67e389330429012", 'N');
-
-insert into ProductCatalogs(PRODUCTDATAID, CATALOGID, OPTIONAL)
- values(459, "afaca8096dd339bb612fb656c046ba08b9fab7b3", 'N');
-insert into ProductCatalogs(PRODUCTDATAID, CATALOGID, OPTIONAL)
- values(460, "afaca8096dd339bb612fb656c046ba08b9fab7b3", 'N');
-
 -- ATI / NVIDIA
 
 insert into ProductCatalogs (PRODUCTDATAID, CATALOGID, OPTIONAL)
@@ -146,5 +55,57 @@ insert into ProductCatalogs (PRODUCTDATAID, CATALOGID, OPTIONAL)
 
 insert into ProductCatalogs (PRODUCTDATAID, CATALOGID, OPTIONAL)
  VALUES(499, "f141229cdd31a8d2e2a5db06d5c6b290f6c77e3f", "N");
+
+-- SLES-10-SP1 i386  BETA Catalog  afb93b054f42082a4c1bdbe060c104dc18ca8ede
+insert into ProductCatalogs(PRODUCTDATAID, CATALOGID, OPTIONAL)
+ values(436, "afb93b054f42082a4c1bdbe060c104dc18ca8ede", 'N');
+insert into ProductCatalogs(PRODUCTDATAID, CATALOGID, OPTIONAL)
+ values(437, "afb93b054f42082a4c1bdbe060c104dc18ca8ede", 'N');
+insert into ProductCatalogs(PRODUCTDATAID, CATALOGID, OPTIONAL)
+ values(438, "afb93b054f42082a4c1bdbe060c104dc18ca8ede", 'N');
+insert into ProductCatalogs(PRODUCTDATAID, CATALOGID, OPTIONAL)
+ values(439, "afb93b054f42082a4c1bdbe060c104dc18ca8ede", 'N');
+
+-- SP2 migration i386
+insert into ProductCatalogs(PRODUCTDATAID, CATALOGID, OPTIONAL)
+ values(805, "a34dd8adbd08655932194d8ea1bce39a32940a4f", 'N');
+insert into ProductCatalogs(PRODUCTDATAID, CATALOGID, OPTIONAL)
+ values(806, "a34dd8adbd08655932194d8ea1bce39a32940a4f", 'N');
+insert into ProductCatalogs(PRODUCTDATAID, CATALOGID, OPTIONAL)
+ values(807, "a34dd8adbd08655932194d8ea1bce39a32940a4f", 'N');
+insert into ProductCatalogs(PRODUCTDATAID, CATALOGID, OPTIONAL)
+ values(808, "a34dd8adbd08655932194d8ea1bce39a32940a4f", 'N');
+
+-- SP2 final "online" i386
+insert into ProductCatalogs(PRODUCTDATAID, CATALOGID, OPTIONAL)
+ values(835, "a34dd8adbd08655932194d8ea1bce39a32940a4f", 'N');
+insert into ProductCatalogs(PRODUCTDATAID, CATALOGID, OPTIONAL)
+ values(836, "a34dd8adbd08655932194d8ea1bce39a32940a4f", 'N');
+insert into ProductCatalogs(PRODUCTDATAID, CATALOGID, OPTIONAL)
+ values(837, "a34dd8adbd08655932194d8ea1bce39a32940a4f", 'N');
+insert into ProductCatalogs(PRODUCTDATAID, CATALOGID, OPTIONAL)
+ values(838, "a34dd8adbd08655932194d8ea1bce39a32940a4f", 'N');
+-- -Updates
+insert into ProductCatalogs(PRODUCTDATAID, CATALOGID, OPTIONAL)
+ values(835, "272d2a8f4388ca27e8a3a2aadde773c0bd293afb", 'N');
+insert into ProductCatalogs(PRODUCTDATAID, CATALOGID, OPTIONAL)
+ values(836, "272d2a8f4388ca27e8a3a2aadde773c0bd293afb", 'N');
+insert into ProductCatalogs(PRODUCTDATAID, CATALOGID, OPTIONAL)
+ values(837, "272d2a8f4388ca27e8a3a2aadde773c0bd293afb", 'N');
+insert into ProductCatalogs(PRODUCTDATAID, CATALOGID, OPTIONAL)
+ values(838, "272d2a8f4388ca27e8a3a2aadde773c0bd293afb", 'N');
+
+-- SP1 final i386
+insert into ProductCatalogs(PRODUCTDATAID, CATALOGID, OPTIONAL)
+ values(820, "272d2a8f4388ca27e8a3a2aadde773c0bd293afb", 'N');
+insert into ProductCatalogs(PRODUCTDATAID, CATALOGID, OPTIONAL)
+ values(821, "272d2a8f4388ca27e8a3a2aadde773c0bd293afb", 'N');
+insert into ProductCatalogs(PRODUCTDATAID, CATALOGID, OPTIONAL)
+ values(822, "272d2a8f4388ca27e8a3a2aadde773c0bd293afb", 'N');
+insert into ProductCatalogs(PRODUCTDATAID, CATALOGID, OPTIONAL)
+ values(823, "272d2a8f4388ca27e8a3a2aadde773c0bd293afb", 'N');
+
+
+
 
 
