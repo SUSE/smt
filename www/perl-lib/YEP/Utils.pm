f45e151b44f795001d22518d7a8eaea6e632c101
Michael Calmer <mc@suse.de>
2008-02-01 14:11:02 +0000

    add lock support in YEP::Utils openLock(<progname>), unlock(<progname>) use lock support in yep-mirror only one mirror pocess at time is allowed.

--- www/perl-lib/YEP/Utils.pm
+++ www/perl-lib/YEP/Utils.pm
@@ -5,6 +5,7 @@ use warnings;
 
 use Config::IniFiles;
 use DBI;
+use Fcntl;
 
 use Locale::gettext ();
 use POSIX ();     # Needed for setlocale()
@@ -51,5 +52,63 @@ sub __ {
     my $domain = "yep";
     return Locale::gettext::dgettext ($domain, $msgid);
 }
+
+#
+# lock file support
+#
+
+#
+# try to create a lock file in /var/run/
+# Return TRUE on success, otherwise FALSE
+#
+sub openLock
+{
+    my $progname = shift;
+    my $pid = $$;
+    
+    my $path = "/var/run/$progname.pid";
+    
+    if( -e $path )
+    {
+        return 0;
+    }
+    sysopen(LOCK, $path, O_WRONLY | O_EXCL | O_CREAT, 0640) or return 0;
+    print LOCK "$pid";
+    close LOCK;
+
+    return 1;
+}
+
+#
+# try to remove the lockfile
+# Return TRUE on success, otherwise false
+#
+sub unLock
+{
+    my $progname = shift;
+    my $pid = $$;
+    
+    my $path = "/var/run/$progname.pid";
+    
+    if(! -e $path )
+    {
+        return 1;
+    }
+    
+    open(LOCK, "< $path") or return 0;
+    my $dp = <LOCK>;
+    close LOCK;
+    
+    if($dp ne "$pid")
+    {
+        return 0;
+    }
+    
+    my $cnt = unlink($path);
+    return 1 if($cnt == 1);
+    
+    return 0;
+}
+
 
 1;
