33cef0ce26b8e12a63db711d194a0be1c8a10797
Michael Calmer <mc@suse.de>
2008-04-24 15:37:23 +0000

    SMT version 0.6.0 database version 0.05 - remove dummy data

---
+++ db/schemas/_common/0.04-0.05/100_clean_tables.sql
@@ -0,0 +1,11 @@
+delete from Catalogs;
+delete from ClientSubscriptions;
+delete from Clients;
+delete from MachineData;
+delete from ProductCatalogs;
+delete from ProductSubscriptions;
+delete from Products;
+delete from Registration;
+delete from Subscriptions;
+delete from Targets;
+
--- www/perl-lib/SMT.pm
+++ www/perl-lib/SMT.pm
@@ -5,6 +5,6 @@ use warnings;
 
 use vars qw($SCHEMA_VERSION);
 
-$SCHEMA_VERSION = 0.04;
+$SCHEMA_VERSION = 0.05;
 
 1;
