321736a0f721213dac0d35a4b0c993ff955f5367
Michael Calmer <mc@suse.de>
2008-06-04 10:13:43 +0000

    init

---
+++ devel-scripts/findProductsWithoutCatalogs.sh
@@ -0,0 +1,4 @@
+#! /bin/sh
+
+echo "select p.PRODUCTDATAID, p.PRODUCT, p.VERSION, p.REL, p.ARCH, p.PRODUCT_LIST from Products p where p.PRODUCTDATAID not in (select distinct pc.PRODUCTDATAID from ProductCatalogs pc) order by p.PRODUCTDATAID" | mysql -usmt -p smt
+
