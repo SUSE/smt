58c9bc35f6aed66a8009ca6a2e2a03135c7097f7
Michael Calmer <mc@suse.de>
2009-02-25 11:11:33 +0000

    fix authentication

--- www/perl-lib/NU/SMTAuth.pm
+++ www/perl-lib/NU/SMTAuth.pm
@@ -3,8 +3,9 @@ package NU::SMTAuth;
 use strict;
 use warnings;
 use SMT::Utils;
+use Data::Dumper;
 
-use Apache2::Const -compile => qw(OK SERVER_ERROR AUTH_REQUIRED);
+use Apache2::Const -compile => qw(OK SERVER_ERROR HTTP_UNAUTHORIZED FORBIDDEN AUTH_REQUIRED);
 
 
 sub handler {
@@ -12,6 +13,8 @@ sub handler {
 
     my $requiredAuth = "none";
     my $cfg = undef;
+    my $requestedPath = $r->uri();
+    
     eval
     {
         $cfg = SMT::Utils::getSMTConfig();
@@ -27,24 +30,28 @@ sub handler {
         $requiredAuth = "none";
     }
 
-    if($requiredAuth eq "none" && substr($r->uri(), -13) ne 'repoindex.xml')
+    if($requiredAuth eq "none" && substr($requestedPath, -13) ne 'repoindex.xml')
     {
-        #$r->log->info("No auth required");
+        $r->log->info("No auth required");
         return Apache2::Const::OK;
     }
 
     my ($status, $password) = $r->get_basic_auth_pw;
     return $status unless $status == Apache2::Const::OK;
 
-    if( substr($r->uri(), -13) eq 'repoindex.xml' )
+    if( substr($requestedPath, -13) eq 'repoindex.xml' )
     {
-        if ( $r->user eq "" ) { return Apache2::Const::AUTH_REQUIRED; }
+        if ( $r->user eq "" ) 
+        { 
+            $r->note_basic_auth_failure;
+            #return Apache2::Const::HTTP_UNAUTHORIZED;
+            return Apache2::Const::AUTH_REQUIRED;
+        }
         #
         # no better check required for repoindex.xml
         # if the user is not valid, he get an empty index file
         return Apache2::Const::OK;
     }
-
 
     #
     # ok, we need to check the credentials
@@ -69,11 +76,13 @@ sub handler {
     if( !exists $existuser->[0] || !defined $existuser->[0] || $existuser->[0] ne $r->user() )
     {
         $r->log->error( "Invalid user: ".$r->user() );
+        $r->note_basic_auth_failure;
         return Apache2::Const::AUTH_REQUIRED;
     }
 
     if($requiredAuth eq "lazy" )
     {
+        $r->log->info("Access granted");
         return Apache2::Const::OK;
     }
     
@@ -85,25 +94,34 @@ sub handler {
     my $targetselect = sprintf("select TARGET from Clients c where c.GUID=%s", $dbh->quote($r->user()) );
     my $target = $dbh->selectcol_arrayref($targetselect);
 
-    my $statement = " select c.LOCALPATH from Catalogs c, ProductCatalogs pc, Registration r ";
-    $catalogselect   .= sprintf(" where r.GUID=%s ", $dbh->quote($r->user()) );
-    $catalogselect   .= " and r.PRODUCTID=pc.PRODUCTDATAID and c.CATALOGID=pc.CATALOGID and c.DOMIRROR like 'Y' ";
+    $statement  = " select c.LOCALPATH from Catalogs c, ProductCatalogs pc, Registration r ";
+    $statement .= sprintf(" where r.GUID=%s ", $dbh->quote($r->user()) );
+    $statement .= " and r.PRODUCTID=pc.PRODUCTDATAID and c.CATALOGID=pc.CATALOGID and c.DOMIRROR like 'Y' ";
     # add a filter by target architecture if it is defined
     if (defined $target && defined ${$target}[0] )
     {
-        $catalogselect .= sprintf(" and c.TARGET=%s", $dbh->quote( ${$target}[0] ));
+        $statement .= sprintf(" and c.TARGET=%s", $dbh->quote( ${$target}[0] ));
     }
 
-    my $localRepoPath = $dbh->selectall_arrayref($statement);
-    foreach my $path ( @{$localRepoPath} )
+    # evil things like "dir/../../otherdir" are solved by apache.
+    # we get here the resulting path
+    # we only need to strip out "somedir////nextdir"
+    $requestedPath = SMT::Utils::cleanPath($requestedPath);
+
+    my $localRepoPath = $dbh->selectall_hashref($statement, 'LOCALPATH');
+    foreach my $path ( keys %{$localRepoPath} )
     {
-        if( index($r->uri(), $path) >= 0 )
+        $r->log->info($r->user()." has access to '$path'");
+        
+        if( index($requestedPath, $path) >= 0 )
         {
+            $r->log->info("Access granted");
             return Apache2::Const::OK;
         }
     }
 
-    $r->log->error("FORBIDDEN: User: ".$r->user()." tried to access: ".$r->uri());
+    $r->log->error("FORBIDDEN: User: ".$r->user()." tried to access: $requestedPath");
+    $r->note_basic_auth_failure;
     return Apache2::Const::FORBIDDEN;
 }
 1;
