afef8cc8a80124a4e71748d2bf667b80a2765037
Jan Kupec <jkupec@suse.cz>
2009-04-27 15:24:08 +0000

    Enforce uniqueness of (CATALOG_ID, TYPE, VALUE) in Filters.

--- db/schemas/mysql/0.10/100-smt-tables.sql
+++ db/schemas/mysql/0.10/100-smt-tables.sql
@@ -91,7 +91,8 @@ create table Catalogs(ID          INT AUTO_INCREMENT UNIQUE KEY,
 create table Filters(ID            INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
                      CATALOG_ID    INT NOT NULL,
                      TYPE          INT NOT NULL DEFAULT 1,
-                     VALUE         VARCHAR(255)
+                     VALUE         VARCHAR(255),
+                     UNIQUE KEY (CATALOG_ID, TYPE, VALUE)
                      -- FOREIGN KEY (CATALOG_ID) REFERENCES Catalogs(ID) ON DELETE CASCADE
                      -- FOREIGN KEY (SUBCATALOG_ID) REFERENCES Subcatalogs(ID) ON DELETE CASCADE
                     );
