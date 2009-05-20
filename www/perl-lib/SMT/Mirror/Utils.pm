bd379b2a27dbf904ac5021a3a3097a0f87ad8f8a
Jan Kupec <jkupec@suse.cz>
2009-05-20 09:49:15 +0000

    copyStatus() added

--- www/perl-lib/SMT/Mirror/Utils.pm
+++ www/perl-lib/SMT/Mirror/Utils.pm
@@ -3,6 +3,8 @@ package SMT::Mirror::Utils;
 use strict;
 use warnings;
 
+use File::Copy;
+
 use SMT::Utils;
 
 =item saveStatus($path)
@@ -29,11 +31,40 @@ sub saveStatus($)
     return 1;
 }
 
-# from path to path
+=item copyStatus($from, $to)
+
+Copy the mirror status file from one repository to another. Use this function
+after creating a testing/production snapshot (another mirror) of the original
+mirror. The status file will serve to check whether the correspoding mirror
+is out of date.
+
+$from and $to must be full local paths of source and target repositories.
+
+Returns 1 if arguments are valid and copying succeeds, 0 otherwise.
+
+$rh = SMT::Repositories::new($dbh);
+$from = $rh->getFullRepoPath($repoid, $cfg);
+$to = $rh->getProductionRepoPath($repoid, $cfg);
+
+# ... create the 'production' snapshot here ...
+
+if ($snapshotcreated)
+{
+    my $success = copyStatus($from, $to);
+    # ...
+}
+
+=cut
 sub copyStatus($$)
 {
     my ($from, $to) = @_;
-   
+
+    # check for .mirror file in full repo
+    return 0 if (not defined $from || not $from || not -e $from.'/.mirror');
+    return 0 if (not defined $to || not $to || not -d $to);
+
+    return 1 if File::Copy::copy($from.'/.mirror', $to.'/.mirror');
+    return 0;
 }
 
 # from path
