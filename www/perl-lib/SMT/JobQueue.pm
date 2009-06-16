7e3d39e06a8cb219b63e7bf3e73d8945959541f2
Thomas Göttlicher <tgoettlicher@suse.de>
2009-06-16 12:47:12 +0000

    added job search to job api

--- www/perl-lib/SMT/JobQueue.pm
+++ www/perl-lib/SMT/JobQueue.pm
@@ -326,6 +326,7 @@ sub deleteJob($)
 {
   my $self = shift;
   my $jobid = shift || return undef;
+  my $guid = shift || return undef;
 
   #TODO: delete job from database
 
@@ -333,6 +334,237 @@ sub deleteJob($)
 };
 
 
+
+
+#
+# small internal function to check if a string is in an array
+#
+sub in_Array($$)
+{
+    my $str = shift || "";
+    my $arr = shift || ();
+
+    foreach my $one (@{$arr}) 
+    {
+        return 1 if $one =~ /^$str$/;
+    }
+    return 0;
+}
+
+
+#
+#
+# create the SQL statement according to the filter
+# this is an internal function
+#
+sub createSQLStatement($$)
+{
+    my $self = shift;
+
+    my $filter = shift || return undef;
+    return undef unless isa($filter, 'HASH');
+
+    my @PROPS = qw(ID PARENT_ID NAME DESCRIPTION TYPE ARGUMENTS STATUS STDOUT STDERR EXITCODE MESSAGE CREATED TARGETED EXPIRES RETRIEVED FINISHED PERSISTENT VERBOSE TIMELAG);    
+    my @ALLPROPS = @PROPS;
+
+    # add > and < properties 
+    my @TMPPROPS = @PROPS;
+    foreach my $prop ( @TMPPROPS )
+    {
+      push (@PROPS, $prop.">");
+      push (@PROPS, $prop."<");
+    }
+
+    my $asXML = ( exists ${$filter}{'asXML'}  &&  defined ${$filter}{'asXML'} ) ? 1 : 0;
+
+
+    # fillup the filter if needed or filter empty
+    if ( scalar( keys %{$filter} ) == 0 ||
+         ( exists ${$filter}{'selectAll'}  &&  defined ${$filter}{'selectAll'} )  )
+    {
+        foreach my $prop (@ALLPROPS)
+        {
+            ${$filter}{$prop} = '' unless (exists ${$filter}{$prop}  &&  defined ${$filter}{$prop} );
+        }
+    }
+
+    my @select = ();
+    my @PSselect = ();
+    my @where = ();
+    my $fromstr  = ' JobQueue jq ';
+
+    # parse the filter hash
+    # collect the select and where statements and quote the input strings
+    foreach my $prop ( @PROPS )
+    {
+        if ( exists ${$filter}{$prop} )
+        {
+	    my $p = $prop;
+	    $p =~ s/\>$//;
+	    $p =~ s/\<$//;
+            push (@select, "$p" );
+            if ( defined ${$filter}{$prop}  &&  ${$filter}{$prop} !~ /^$/ )
+            {
+		if ( $prop =~ /.*\>/ )
+		{
+                  push( @where, " jq.$p > " . $self->{'dbh'}->quote(${$filter}{$prop}) . ' ' );
+		}
+		elsif ( $prop =~ /.*\</ )
+		{
+                  push( @where, " jq.$p < " . $self->{'dbh'}->quote(${$filter}{$prop}) . ' ' );
+		}
+		else
+		{
+                  push( @where, " jq.$prop LIKE " . $self->{'dbh'}->quote(${$filter}{$prop}) . ' ' );
+		}
+            }        
+        }
+    }
+
+    # add query for guid if defined
+    if ( exists ${$filter}{'GUID'} )
+    {
+        $fromstr .= ' LEFT JOIN Clients cl ON ( jq.GUID_ID = cl.ID ) ';
+        push (@select, "cl.GUID" );
+      
+	if ( defined ${$filter}{'GUID'}  &&  ${$filter}{'GUID'} !~ /^$/ )
+	{
+                push( @where, " cl.GUID LIKE " . $self->{'dbh'}->quote(${$filter}{'GUID'}) . ' ' );
+	}
+    }
+
+ 
+    # make sure the ID is in the select statement in any case
+    push( @select, "ID" ) unless ( in_Array("ID", \@select) );
+    # if XML gets exported then switch to lower case attributes
+    my @selectExpand = ();
+    foreach my $sel (@select)
+    {
+	if ( $sel =~ /\./ )
+	{
+	  my $s = $sel;
+	  $s =~ s/^.*\.//;
+          push (@selectExpand,  "  $sel as ".( $asXML ? lc($s):$s ).'  ' );
+	}
+	else
+	{
+          push (@selectExpand,  "  jq.$sel as ".( $asXML ? lc($sel):$sel ).'  ' );
+	}
+    }
+
+    my $selectstr = join(', ', @selectExpand) || return undef;
+    if ( @PSselect > 0 )
+    {
+        $selectstr   .= ' ,  '.join(', ', @PSselect);
+    }
+
+    $fromstr      = $fromstr || return undef;
+    my $wherestr  = join(' AND ', @where) || ' 1 '; # create 'where 1' if $wherestr is empty
+
+    return " select  $selectstr  from  $fromstr  where  $wherestr ";
+}
+
+
+#
+# perform a select for job information - internal function
+#
+sub getJobsInfo_internal($)
+{
+    my $self = shift;
+
+    my $filter = shift || {};
+    return undef unless ( isa($filter, 'HASH') );
+
+    # let create the SQL statement
+    my $sql = $self->createSQLStatement($filter);
+    return undef unless defined $sql;
+
+    my $asXML = ( exists ${$filter}{'asXML'} &&  defined ${$filter}{'asXML'} ) ? 1:0;
+
+    ## NOTE: This can be used for testing/debugging
+    ## NOTE: will only return the generated SQL statement but not evaluate it
+    #return $sql;
+
+    my $refKey = $asXML ? 'id':'ID';
+    my $result = $self->{'dbh'}->selectall_hashref($sql, $refKey);
+
+    if ( $asXML )
+    {
+        if ( keys %{$result} == 1  &&  ${$filter}{'asXML'} eq 'one' )
+        {
+            my @keys = keys %{$result};
+            return XMLout( ${$result}{$keys[0]}
+                      , rootname => "job"
+                      , xmldecl => '<?xml version="1.0" encoding="UTF-8" ?>' );
+        }
+        else
+        {
+            my @jobList = ();
+            foreach my $key ( keys %{$result} )
+            {
+                push ( @jobList, ${$result}{$key} );
+            }
+
+            my $jobsHash = {  'job' => [@jobList]  };
+            return XMLout( $jobsHash
+                          , rootname => "jobs"
+                          , xmldecl => '<?xml version="1.0" encoding="UTF-8" ?>' );
+        }
+    }
+    else
+    {
+        return $result;
+    }
+}
+
+
+
+
+#
+# getClientsInfo
+#   query all jobs about information, filtered by filter
+#   this is the mose generic function to query jobs (besides the internal one)
+#   parameters
+#    $self
+#    $filter (hash) : filter for the query
+#
+sub getJobsInfo($$)
+{
+    my $self = shift;
+    my $filter = shift || {};
+
+    return undef unless ( isa($filter, 'HASH') );  
+    return $self->getJobsInfo_internal($filter);
+}
+
+
+#
+# getAllJobInfo
+#   get detailled information about all jobs with all information
+#   parameter
+#    self
+#
+sub getAllJobsInfo($)
+{
+    my $self = shift;
+
+    # emtpy filter means: select all information
+    return $self->getJobsInfo_internal({});
+}
+
+
+#
+# getAllJobsInfoAsXML
+#   get detailled information about all jobs with all information as XML
+#   parameter
+#    self
+sub getAllJobsInfoAsXML($)
+{
+    my $self = shift;
+
+    # emtpy filter means: select all information
+    return $self->getJobsInfo_internal({ 'asXML' => '', 'selectAll' => '' });
+}
 
 
 1;
