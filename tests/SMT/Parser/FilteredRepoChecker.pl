5c040461b5967dabdba745cb759039e3da39fafe
Jan Kupec <jkupec@suse.cz>
2009-06-17 13:28:33 +0000

    improved checker test

--- tests/SMT/Parser/FilteredRepoChecker.pl
+++ tests/SMT/Parser/FilteredRepoChecker.pl
@@ -5,7 +5,7 @@ BEGIN {
 }
 
 use strict;
-use Test::Simple tests => 1;
+use Test::Simple tests => 6;
 use Cwd;
 
 #use SMT::Utils;
@@ -20,14 +20,28 @@ my $wd = Cwd::cwd();       # current working dir
 #my $vblevel = LOG_DEBUG|LOG_DEBUG2|LOG_WARN|LOG_ERROR|LOG_INFO1|LOG_INFO2;
 
 my $checker = SMT::Parser::FilteredRepoChecker->new(); #log => $log, vblevel => $vblevel); 
-$checker->repoPath($wd . '/testdata/rpmmdtest/code11repo'); # FIXME this should use the testdir
+$checker->repoPath($wd . '/testdata/rpmmdtest/sharedpkgs'); # FIXME this should use the testdir
 
 my $filter = SMT::Filter->new();
-$filter->add(SMT::Filter->TYPE_NAME_VERSION, 'logwatch-658');
+$filter->add(SMT::Filter->TYPE_NAME_VERSION, 'imap-368');
 $checker->filter($filter);
 
 my ($result, $problems, $causes) = $checker->check();
 
-ok ($result);
+#print Dumper($result, $problems, $causes);
 
-# TODO: needs better test case repo (for negative case)
+ok (!$result);
+
+ok (exists $problems->{'imap-lib-2006c1_suse-127.1'} &&
+    $problems->{'imap-lib-2006c1_suse-127.1'} eq 'testsharedpkg-999');
+
+ok (exists $causes->{'imap-lib-2006c1_suse-127.1'} &&
+    $causes->{'imap-lib-2006c1_suse-127.1'} eq 'imap-368');
+
+$filter->remove(SMT::Filter->TYPE_NAME_VERSION, 'imap-368');
+$filter->add(SMT::Filter->TYPE_NAME_VERSION, 'logwatch-658');
+($result, $problems, $causes) = $checker->check();
+
+ok ($result);
+ok (!%$problems);
+ok (!%$causes);
