ecfb39d47148b61ff145b0a2b7a9857fd576e31c
J. Daniel Schmidt <jdsn@suse.de>
2009-07-08 09:51:21 +0000

    install as well the scripts

--- devel-scripts/quickRESTUpdate.pl
+++ devel-scripts/quickRESTUpdate.pl
@@ -8,6 +8,9 @@ cp -av $PRE/www/perl-lib/SMT/Client/Auth.pm  /srv/www/perl-lib/SMT/Client/
 cp -av $PRE/www/perl-lib/SMT/Job[Q\.]*pm  /usr/lib/perl5/vendor_perl/5.10.0/SMT/
 cp -av $PRE/www/perl-lib/SMT/Client.pm  /usr/lib/perl5/vendor_perl/5.10.0/SMT/
 cp -av $PRE/www/perl-lib/SMT.pm  /usr/lib/perl5/vendor_perl/5.10.0/
+cp -av $PRE/script/smt*  /usr/sbin/
+chmod 755 /usr/sbin/smt*
+
 
 # enable to restart apache after copying new files
 # rcapache2 restart
