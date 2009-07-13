761384811957549586a15d332f26374523f9e89a
J. Daniel Schmidt <jdsn@suse.de>
2009-07-13 15:51:53 +0000

    install also cron scripts

--- devel-scripts/quickRESTUpdate.pl
+++ devel-scripts/quickRESTUpdate.pl
@@ -9,6 +9,7 @@ cp -av $PRE/www/perl-lib/SMT/Job[Q\.]*pm  /usr/lib/perl5/vendor_perl/5.10.0/SMT/
 cp -av $PRE/www/perl-lib/SMT/Client.pm  /usr/lib/perl5/vendor_perl/5.10.0/SMT/
 cp -av $PRE/www/perl-lib/SMT.pm  /usr/lib/perl5/vendor_perl/5.10.0/
 cp -av $PRE/script/smt*  /usr/sbin/
+cp -av $PRE/cron/smt* /usr/lib/SMT/bin/
 chmod 755 /usr/sbin/smt*
 
 
