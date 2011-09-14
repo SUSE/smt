762e46fdaf7cd10878af69831ff02672f6d91242
Michael Calmer <mc@suse.de>
2011-09-14 10:43:26 +0200

    update performance tools

--- performance2/launch.sh
+++ performance2/launch.sh
@@ -4,14 +4,20 @@ free=$(free | grep ^Mem: | awk '{ print $4; }')
 maxclients=$[ $free / 10240 ]
 progname=${0##*/}
 
-if [ -z "$1" -o -z "$2" -o "$1" = "-h" ]; then 
-   echo "usage: $progname <number of concurrent clients> <number of registrations per client>"
+if [ -z "$1" -o -z "$2" -o "$1" = "-h" ]; then
+   echo "usage: $progname <number of concurrent clients> <number of registrations per client> [<guid prefix>] [<suse_register options>]"
    exit 1
 fi
 
 concurrency=$1
 registrations=$2
-options=$3
+if [ -z "$4" ]; then
+    options=$3
+    guidprefix="a"
+else
+    guidprefix=$3
+    options=$4
+fi
 
 if [ $maxclients -lt $concurrency ]; then
    echo "too much clients requested (max=$maxclients) ... aborting."
@@ -27,7 +33,7 @@ do
         start=$[ $i * $registrations ]
         end=$[ $start + $registrations - 1 ]
         ln -sf ../suse_register .
-        echo "launching ../repeatedStart.pl $start $end $options"
-	../repeatedStart.pl $start $end "$options" &
+        echo "launching ../repeatedStart.pl $start $end $guidprefix $options"
+	../repeatedStart.pl $start $end "$guidprefix" "$options" &
 	popd > /dev/null || continue
 done 2>&1 | tee log
