aef24227ea042990f6bb301bb6159c6956b4d8dc
Michael Calmer <mc@suse.de>
2012-12-13 10:37:07 +0100

    reschedule also the gen-report job

--- script/reschedule-sync.sh
+++ script/reschedule-sync.sh
@@ -4,6 +4,7 @@
 # cron job hour is 0 or 1: than it is needed
 
 JOBNAME="smt-daily"
+REPORT="smt-gen-report"
 CRONPATH="/etc/smt.d/novell.com-smt"
 
 if [ ! -r "$CRONPATH" ]; then
@@ -30,13 +31,18 @@ fi
 # MINUTE between 0 - 59
 NEW_MINUTE=`echo "$RANDOM % 60" | bc`
 
+# Report between 4 - 7 am
+REPORT_HOUR=`echo "$RANDOM % 3 + 4" | bc`
+REPORT_MINUTE=`echo "$RANDOM % 60" | bc`
+
 TMP_FILE=`mktemp "${CRONPATH}.XXXXXX" 2>/dev/null`
 if [ ! -e $TMP_FILE ]; then
     echo "unable to create tmp file" >&2
     exit 1
 fi
-grep -v $JOBNAME $CRONPATH > $TMP_FILE
+grep -v -E "$JOBNAME|$REPORT" $CRONPATH > $TMP_FILE
 echo "$NEW_MINUTE $NEW_HOUR * * * root /usr/lib/SMT/bin/smt-daily" >> $TMP_FILE
+echo "$REPORT_MINUTE $REPORT_HOUR * * 1 root /usr/lib/SMT/bin/smt-gen-report" >> $TMP_FILE
 
 STAMP=`date +%Y%m%d%H%M%S`
 mv "$CRONPATH" "$CRONPATH.$STAMP"
