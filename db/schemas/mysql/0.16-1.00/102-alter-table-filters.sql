86cc6a505d593ef45dcdeefce28181ab2980b9c3
Michael Calmer <mc@suse.de>
2014-02-05 13:59:13 +0100

    drop index only if it exists (bnc#852632)

--- db/schemas/mysql/0.16-1.00/102-alter-table-filters.sql
+++ db/schemas/mysql/0.16-1.00/102-alter-table-filters.sql
@@ -1,5 +1,25 @@
 
 alter table Filters add column STAGINGGROUP_ID INT NOT NULL DEFAULT 1;
-alter table Filters drop key CATALOG_ID;
+
+DELIMITER $$
+
+DROP PROCEDURE IF EXISTS smt.drop_index_if_exists $$
+CREATE PROCEDURE smt.drop_index_if_exists(in theTable varchar(128), in theIndexName varchar(128) )
+BEGIN
+ IF((SELECT COUNT(*) AS index_exists
+       FROM information_schema.statistics
+      WHERE TABLE_SCHEMA = DATABASE()
+        AND table_name = theTable
+        AND index_name = theIndexName) > 0) THEN
+   SET @s = CONCAT('DROP INDEX ' , theIndexName , ' ON ' , theTable);
+   PREPARE stmt FROM @s;
+   EXECUTE stmt;
+ END IF;
+END $$
+
+DELIMITER ;
+
+call smt.drop_index_if_exists('Filters', 'CATALOG_ID');
+
 alter table Filters add unique key CATALOG_ID (CATALOG_ID, STAGINGGROUP_ID, TYPE, VALUE);
 
