07c1c6a7c558c0d8d8645c07d4426d98a0b53311
Michael Calmer <mc@suse.de>
2014-03-04 16:44:36 +0100

    fix alter catalogs

--- db/schemas/mysql/1.00-2.00/120-alter-catalogs.sql
+++ db/schemas/mysql/1.00-2.00/120-alter-catalogs.sql
@@ -1,24 +1,6 @@
 
-DELIMITER $$
-
-DROP PROCEDURE IF EXISTS smt.drop_index_if_exists $$
-CREATE PROCEDURE smt.drop_index_if_exists(in theTable varchar(128), in theIndexName varchar(128) )
-BEGIN
- IF((SELECT COUNT(*) AS index_exists
-       FROM information_schema.statistics
-      WHERE TABLE_SCHEMA = DATABASE()
-        AND table_name = theTable
-        AND index_name = theIndexName) > 0) THEN
-   SET @s = CONCAT('DROP INDEX ' , theIndexName , ' ON ' , theTable);
-   PREPARE stmt FROM @s;
-   EXECUTE stmt;
- END IF;
-END $$
-
-DELIMITER ;
-
 ALTER TABLE Catalogs drop PRIMARY KEY;
 
-call smt.drop_index_if_exists('Catalogs', 'ID');
-
 ALTER TABLE Catalogs ADD PRIMARY KEY ID(ID);
+
+DROP INDEX ID ON Catalogs;
