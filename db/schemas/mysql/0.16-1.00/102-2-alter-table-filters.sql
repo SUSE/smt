39d32bb3d587838f655599cc01370a80716d6dfc
Michael Calmer <mc@suse.de>
2014-07-23 10:07:18 +0200

    fix creating stored proceedure with DBIx (bnc#884574)

--- db/schemas/mysql/0.16-1.00/102-2-alter-table-filters.sql
+++ db/schemas/mysql/0.16-1.00/102-2-alter-table-filters.sql
@@ -1,7 +1,7 @@
+-- perl DBI exec statement by statement and do not need the DELIMITER directive
+DROP PROCEDURE IF EXISTS smt.drop_index_if_exists;
 
-DELIMITER $$
-
-DROP PROCEDURE IF EXISTS smt.drop_index_if_exists $$
+-- use -- after ; to trick DBIx to not split statement after ;
 CREATE PROCEDURE smt.drop_index_if_exists(in theTable varchar(128), in theIndexName varchar(128) )
 BEGIN
  IF((SELECT COUNT(*) AS index_exists
@@ -9,13 +9,11 @@ BEGIN
       WHERE TABLE_SCHEMA = DATABASE()
         AND table_name = theTable
         AND index_name = theIndexName) > 0) THEN
-   SET @s = CONCAT('DROP INDEX ' , theIndexName , ' ON ' , theTable);
-   PREPARE stmt FROM @s;
-   EXECUTE stmt;
- END IF;
-END $$
-
-DELIMITER ;
+   SET @s = CONCAT('DROP INDEX ' , theIndexName , ' ON ' , theTable); --
+   PREPARE stmt FROM @s; --
+   EXECUTE stmt; --
+ END IF; --
+END;
 
 call smt.drop_index_if_exists('Filters', 'CATALOG_ID');
 
