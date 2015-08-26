d7024bb5ca885efd7c933377c627e64b01933028
Michael Calmer <mc@suse.de>
2015-08-26 15:40:45 +0200

    add index on ProductMigrations

--- db/schemas/mysql/2.04-2.05/100-create-ProductMigrations.sql
+++ db/schemas/mysql/2.04-2.05/100-create-ProductMigrations.sql
@@ -3,5 +3,6 @@ create table ProductMigrations (
     SRCPDID   integer NOT NULL,
     TGTPDID   integer NOT NULL,
     SRC       CHAR(1) DEFAULT 'S',
-    UNIQUE pdid_migid_unq (SRCPDID, TGTPDID)
+    UNIQUE pdid_migid_unq (SRCPDID, TGTPDID),
+    INDEX ProductMigrations_srcpdid_tgtpdid_src_idx (SRCPDID, TGTPDID, SRC)
 );
--- db/schemas/mysql/2.05/100-smt-tables.sql
+++ db/schemas/mysql/2.05/100-smt-tables.sql
@@ -167,7 +167,8 @@ create table ProductMigrations (
     SRCPDID   integer NOT NULL,
     TGTPDID   integer NOT NULL,
     SRC       CHAR(1) DEFAULT 'S',
-    UNIQUE pdid_migid_unq (SRCPDID, TGTPDID)
+    UNIQUE pdid_migid_unq (SRCPDID, TGTPDID),
+    INDEX ProductMigrations_srcpdid_tgtpdid_src_idx (SRCPDID, TGTPDID, SRC)
 );
 
 create table ProductCatalogs(PRODUCTID   integer NOT NULL,
