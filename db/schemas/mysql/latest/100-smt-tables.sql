2a7729a302ea0625b720324ff5c5f12106abf1f4
Michael Calmer <mc@suse.de>
2015-08-26 13:49:50 +0200

    create procedure drop_index_if_exists

--- db/schemas/mysql/latest/100-smt-tables.sql
+++ db/schemas/mysql/latest/100-smt-tables.sql
@@ -18,6 +18,8 @@ drop table if exists RepositoryContentData;
 
 drop table if exists StagingGroups;
 
+DROP PROCEDURE IF EXISTS smt.drop_index_if_exists;
+
 -- schema version is inserted by smt-db script according to $SMT::SCHEMA_VERSION
 CREATE TABLE migration_schema_version (
           name varchar(128) NOT NULL,
@@ -313,4 +315,17 @@ create table Packages ( ID          INTEGER UNSIGNED NOT NULL AUTO_INCREMENT,
                         EXTLOCATION VARCHAR(255) NOT NULL,
                         CONSTRAINT Packages_id_pk PRIMARY KEY (ID)
                       ) ENGINE=InnoDB;
+
+CREATE PROCEDURE smt.drop_index_if_exists(in theTable varchar(128), in theIndexName varchar(128) )
+BEGIN
+ IF((SELECT COUNT(*) AS index_exists
+       FROM information_schema.statistics
+      WHERE TABLE_SCHEMA = DATABASE()
+        AND table_name = theTable
+        AND index_name = theIndexName) > 0) THEN
+   SET @s = CONCAT('DROP INDEX ' , theIndexName , ' ON ' , theTable);
+   PREPARE stmt FROM @s;
+   EXECUTE stmt;
+ END IF;
+END;
 
