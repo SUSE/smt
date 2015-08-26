62f57e756ace0cc77fce4e45dab2065e3785b507
Michael Calmer <mc@suse.de>
2015-08-26 13:37:34 +0200

    keep migration the same as during a new setup

--- db/schemas/mysql/2.04-2.05/100-create-ProductMigrations.sql
+++ db/schemas/mysql/2.04-2.05/100-create-ProductMigrations.sql
@@ -3,5 +3,5 @@ create table ProductMigrations (
     SRCPDID   integer NOT NULL,
     TGTPDID   integer NOT NULL,
     SRC       CHAR(1) DEFAULT 'S',
-    UNIQUE pdid_migid_unq (SRCPDID, TGTPDID)
+    UNIQUE INDEX ProductExtensions_pdid_migid_unq (SRCPDID, TGTPDID)
 );
