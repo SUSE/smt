9084949cf65805754f3ef92131bf846c7eb7e25d
Jiri Srain <jsrain@suse.cz>
2015-12-09 15:00:14 +0100

    Replaced the repository filter (fate#319777)

--- yast/src/include/smt/wizard.rb
+++ yast/src/include/smt/wizard.rb
@@ -292,15 +292,13 @@ module Yast
         Builtins.y2error("Unknown sequence_type: %1", sequence_type)
       end
 
-      contents = VBox("tab")
-      w = CWM.CreateWidgets(
-        ["tab"],
-        Convert.convert(
-          wd,
-          :from => "map <string, any>",
-          :to   => "map <string, map <string, any>>"
-        )
-      )
+      wd["timeout"] = {
+        "ui_timeout"    => 2000,
+        "widget"     => :empty
+      }
+
+      contents = VBox("tab", "timeout")
+      w = CWM.CreateWidgets(["tab", "timeout"], wd)
       contents = CWM.PrepareDialog(contents, w)
 
       Wizard.SetContentsButtons(
