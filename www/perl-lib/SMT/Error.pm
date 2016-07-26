b08e156c3ad3f9a172f720783051e6043187b5de
Michael Calmer <mc@suse.de>
2016-07-26 16:32:09 +0200

    Add a generic error handler to return json format (bsc#943355)

---
+++ www/perl-lib/SMT/Error.pm
@@ -0,0 +1,46 @@
+package SMT::Error;
+
+use SMT::Utils;
+use Apache2::Const -compile => qw(:log OK SERVER_ERROR NOT_FOUND);
+use JSON;
+
+#
+# Apache Handler
+# this is the main function of this request handler
+#
+sub handler {
+    my $r = shift;
+    # only /connect pages get json response errors
+    if ($r->prev() && $r->prev()->uri() =~ /\/connect\//)
+    {
+        $r->content_type('application/json');
+        if ($r->path_info() eq "/404")
+        {
+            # tell the browser the error
+            $r->log->error("Page Not Found");
+            $r->status(Apache2::Const::NOT_FOUND);
+            $r->custom_response(Apache2::Const::NOT_FOUND, "");
+            print encode_json(
+              { 'error' => "Page Not Found: Please contact your Administrator",
+                'localized_error' => "Page Not Found: Please contact your Administrator",
+                'status' => Apache2::Const::NOT_FOUND }
+            );
+        }
+        else
+        {
+            # tell the browser the error
+            $r->log->error("Internal Server Error");
+            $r->status(Apache2::Const::SERVER_ERROR);
+            $r->custom_response(Apache2::Const::SERVER_ERROR, "");
+            print encode_json(
+              { 'error' => "Internal Server Error: Please contact your Administrator",
+                'localized_error' => "Internal Server Error: Please contact your Administrator",
+                'status' => Apache2::Const::SERVER_ERROR }
+            );
+        }
+        # tell apache engine that everything is OK to not print the HTML standard error page
+        return Apache2::Const::OK
+    }
+}
+
+1;
