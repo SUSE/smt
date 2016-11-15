40c4591e2f53d0e554f0f15469bae31285225033
Michael Calmer <mc@suse.de>
2016-11-15 10:10:50 +0100

    Check for valid custom repository id (bsc#1004716)

--- www/perl-lib/SMT/CLI.pm
+++ www/perl-lib/SMT/CLI.pm
@@ -1466,6 +1466,12 @@ sub setupCustomCatalogs
     my ($cfg, $dbh) = init();
     my $affected = 0;
 
+    if (!SMT::Utils::lookupCatalogIdByDataId($dbh, $options{catalogid}, 'C'))
+    {
+        print $options{catalogid}." is not a valid custom repository\n";
+        return 0;
+    }
+
     if(! $options{addproducts})
     {
         # delete existing catalogs with this id
